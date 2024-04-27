ce103de2a27a35a5faddc8a0afa0a809e56b63d2
Lostchemist <53584459+Lostchemist@users.noreply.github.com>
2024-04-27 10:48:42 +0200

    corrections to pass all the tests, all 4 tests give ok

--- .ipynb_checkpoints/vikingsClasses-checkpoint.py
+++ .ipynb_checkpoints/vikingsClasses-checkpoint.py
@@ -1,19 +1,15 @@
 import random
-
 # Soldier
 
 class Soldier:
     def __init__(self, health, strength):
         self.health = health
         self.strength = strength
-        
     def attack(self):
-        self.strength = strength
-        return self.strength
         
+        return self.strength
     def receiveDamage(self, damage):
-        self.health -= damage
-    
+        self.health -= damage  
 
 # Viking
 
@@ -21,102 +17,103 @@ class Viking(Soldier):
     def __init__(self, name, health, strength):
         self.name = name
         #Health and Strengh son variables heredadas del padre
-        super().__init__(health)
-        super().__init__(strength)
+        super().__init__(health, strength)
+        # super().__init__(strength)
 
     def attack(self):
-        return "Odin Owns You All!"
+         
+        return self.strength
     
     def battleCry(self):
-        return "¡Odin os posee a todos!"
+        return "Odin Owns You All!"
 
     def receiveDamage(self, damage):
-        self.health -= damage
+        self.health = self.health - damage
         if self.health > 0:
-            return f"{self.name} ha recibido {damage} puntos de daño"
+            return f"{self.name} has received {damage} points of damage"
         else:
-            return f"{self.name} ha muerto en acto de combate"
+            return f"{self.name} has died in act of combat"
 
 # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        super().__init__(health)
-        super().__init__(strength)
+        super().__init__(health, strength)
+        # super().__init__(strength)
         
 
     def receiveDamage(self, damage):
         self.health -= damage
         if self.health > 0:
-            return f"Un 'Saxon' ha recibido {damage} puntos de daño"
+            return f"A Saxon has received {damage} points of damage"
         else:
-            return f"Un 'Saxon' ha muerto en combate"
+            return f"A Saxon has died in combat"
 
 # Davicente
 
 class War():
     def __init__(self):
-        vikingArmy = []
-        saxonArmy = []
+        self.vikingArmy = []
+        self.saxonArmy = []
 
     def addViking(self, viking):
-        vikingArmy.append(viking)
+        self.vikingArmy.append(viking)
     
     def addSaxon(self, saxon):
-        saxonArmy.append(saxon)
+        self.saxonArmy.append(saxon)
     
     def vikingAttack(self):
         #Guardamos las instancias de vikings y saxons en las listas correspondientes
-        elegidoSaxon = random.choice(saxonArmy)
-        elegidoViking = random.choice(vikingArmy)
+        self.elegidoSaxon = random.choice(self.saxonArmy)
+        self.elegidoViking = random.choice(self.vikingArmy)
         #Invocamos el methodo recieveDamage sobre el saxon elegido con methodo random
-        elegidoSaxon.recieveDamage(elegidoViking.strength)
+        self.elegidoSaxon.receiveDamage(self.elegidoViking.strength)
         #Comprobamos si aun esta vivo el saxon
-        if elegidoSaxo.health  < 1:
-            saxonArmy.remove(saxonElegido)
-        return elegidoViking.strength
+        if self.elegidoSaxon.health  < 1:
+            self.saxonArmy.remove(self.elegidoSaxon)
+        #return self.elegidoViking.strength
     
     def saxonAttack(self):
         #Guardamos las instancias de vikings y saxons en las listas correspondientes
-        elegidoSaxon = random.choice(saxonArmy)
-        elegidoViking = random.choice(vikingArmy)
+        self.elegidoSaxon = random.choice(self.saxonArmy)
+        self.elegidoViking = random.choice(self.vikingArmy)
         #Invocamos el methodo recieveDamage sobre el viking elegido con methodo random
-        elegidoViking.recieveDamage(elegidoSaxon.strength)
+        self.elegidoViking.receiveDamage(self.elegidoSaxon.strength)
         #Comprobamos si aun esta vivo el Viking
-        if elegidoViking.health  < 1:
-            vikingArmy.remove(elegidoViking)
-        return elegidoSaxon.strength
+        if self.elegidoViking.health  < 1:
+            self.vikingArmy.remove(self.elegidoViking)
+        #return self.elegidoSaxon.strength
 
     def showStatus(self):
-        if vikingArmy.len()==0:
-            return f"Los Sajones han luchado por sus vidas y sobreviven otro día..."
-        elif saxonArmy.len()==0:
-            return f"¡Los Vikingos han ganado la guerra del siglo!"
+        if len(self.vikingArmy)==0:
+            return f"Saxons have fought for their lives and survive another day..."
+        elif len(self.saxonArmy)==0:
+            return f"Vikings have won the war of the century!"
         else:
-            return f"Los Vikingos y los Sajones todavía están en plena batalla."
+            return f"Vikings and Saxons are still in the thick of battle."
     pass
 
-#yop
-class War2:
+# #yop
+# class War2:
 
-    def __init__(self):
-        # your code here
+#     def __init__(self):
+#         # your code here
 
-    def addViking(self, Viking):
-        # your code here
+#     def addViking(self, Viking):
+#         # your code here
     
-    def addSaxon(self, Saxon):
-        # your code here
+#     def addSaxon(self, Saxon):
+#         # your code here
     
-    def vikingAttack(self):
-        # your code here
+#     def vikingAttack(self):
+#         # your code here
 
-    def saxonAttack(self):
-        # your code here
+#     def saxonAttack(self):
+#         # your code here
 
-    def showStatus(self):
-        # your code here
+#     def showStatus(self):
+#         # your code here
 
-    pass
+#     pass
 
 
